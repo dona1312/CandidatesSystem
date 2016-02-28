c5a2163d859e67d414c9ea03b035b7f1e31fe258
zdravkastrahinova <zdravkastrahinova1022@gmail.com>
2016-02-28 17:55:15 +0200

    Add validation attributes and add search styles

--- ITSystem/ITSystem/ViewModels/Candidates/CandidateEditVM.cs
+++ ITSystem/ITSystem/ViewModels/Candidates/CandidateEditVM.cs
@@ -12,22 +12,25 @@ namespace ITSystem.ViewModels.Candidates
         public int Id { get; set; }
 
         [Required(ErrorMessage="Please input first name! It is required!")]
-        [StringLength(70, MinimumLength=3, ErrorMessage= "First name must contain between 3 and 70 letters")]
-        [RegularExpression(@"^([A-z -])+$", ErrorMessage= "First name can consist only letters, spaces and dashes")]
+        [StringLength(70, MinimumLength=3, ErrorMessage="First name must contain between 3 and 70 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "First name can consist only letters, spaces and dashes")]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
 
-        [Required(ErrorMessage = "Please input middle name! It is required!")]
-        [StringLength(70, MinimumLength = 3, ErrorMessage = "Middle name must contain between 3 and 70 letters")]
-        [RegularExpression(@"^([A-z -])+$", ErrorMessage = "Middle name can consist only letters, spaces and dashes")]
+        [Required(ErrorMessage="Please input middle name! It is required!")]
+        [StringLength(70, MinimumLength=3, ErrorMessage="Middle name must contain between 3 and 70 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "Middle name can consist only letters, spaces and dashes")]
+        [Display(Name="Middle Name")]
         public string MiddleName { get; set; }
 
-        [Required(ErrorMessage = "Please input last name! It is required!")]
-        [StringLength(70, MinimumLength = 3, ErrorMessage = "Last name must contain between 3 and 70 letters")]
-        [RegularExpression(@"^([A-z -])+$", ErrorMessage = "Last name can consist only letters, spaces and dashes")]
+        [Required(ErrorMessage="Please input last name! It is required!")]
+        [StringLength(70, MinimumLength=3, ErrorMessage="Last name must contain between 3 and 70 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "Last name can consist only letters, spaces and dashes")]
+        [Display(Name="Last Name")]
         public string LastName { get; set; }
 
-        [Required(ErrorMessage="Please input email!It is required!")]
-        [StringLength(100, MinimumLength=5, ErrorMessage = "Email nust contain between 5 and 100 characters")]
+        [Required(ErrorMessage="Please input email! It is required!")]
+        [StringLength(100, MinimumLength=5, ErrorMessage="Email must contain between 5 and 100 characters")]
         [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
         ErrorMessage = "Please enter correct email address")]
         public string Email { get; set; }
--- ITSystem/ITSystem/ViewModels/Consultants/ConsultantEditVM.cs
+++ ITSystem/ITSystem/ViewModels/Consultants/ConsultantEditVM.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using ITSystem.Enums;
 
 namespace ITSystem.ViewModels.Consultants
@@ -9,11 +10,34 @@ namespace ITSystem.ViewModels.Consultants
     public class ConsultantEditVM
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage="Please input username! It is required!")]
+        [StringLength(50, MinimumLength=3, ErrorMessage="Username must contain between 3 and 50 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "Username can consist only letters, spaces and dashes")]
         public string Username { get; set; }
+
+        [Required(ErrorMessage="Please input password! It is required!")]
+        [StringLength(50, MinimumLength=3, ErrorMessage="Password must contain between 3 and 50 letters")]
         public string Password { get; set; }
+
+        [Required(ErrorMessage="Please input first name! It is required!")]
+        [StringLength(50, MinimumLength=3, ErrorMessage="First name must contain between 3 and 50 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "First name can consist only letters, spaces and dashes")]
+        [Display(Name="First Name")]
         public string FirstName { get; set; }
+
+        [Required(ErrorMessage="Please input last name! It is required!")]
+        [StringLength(50, MinimumLength=3, ErrorMessage="Last name must contain between 3 and 50 letters")]
+        [RegularExpression(@"^([A-z -]+)$", ErrorMessage = "Lastn name can consist only letters, spaces and dashes")]
+        [Display(Name="Last Name")]
         public string LastName { get; set; }
+
+        [Required(ErrorMessage="Please input email! It is required!")]
+        [StringLength(100, MinimumLength=5, ErrorMessage="Email must contain between 5 and 100 characters")]
+        [RegularExpression(@"\A(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)\Z",
+        ErrorMessage="Please enter correct email address")]
         public string Email { get; set; }
+
         public RankEnum Rank { get; set; }
     }
 }
